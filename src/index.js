4f4f82951c63006ca5d124e5df81766bd2c22155
Angelin Sneha V <angelin.sneha@blueoptima.com>
2024-09-19 13:18:34 +0530

    delete unwanted folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import Button from '../src/stories/Button'
 import Header from '../src/stories/Header'
 
